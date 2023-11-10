fd7ebf48fd2ea1b0e27292610110298174352818
kandimallavaishnavi1704 <150412117+kandimallavaishnavi1704@users.noreply.github.com>
2023-11-10 12:22:56 -0800

    Update Vkandimalla.py

--- Vkandimalla.py
+++ Vkandimalla.py
@@ -1,2 +1,4 @@
-
-print("This is my python program in github")
+user_num = int(input("Enter a number: "))
+squared_num = user_num ** 2
+print(f"The square of {user_num} is {squared_num}")
+print("The pen is mightier than the sword!")
