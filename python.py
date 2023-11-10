d55ecb7e42d9e31ee54f78f8d380feb17b7ac343
kandimallavaishnavi1704 <150412117+kandimallavaishnavi1704@users.noreply.github.com>
2023-11-10 12:38:19 -0800

    python.py

---
+++ python.py
@@ -0,0 +1,9 @@
+import math 
+
+print("Enter a number:")
+num = int(input())
+
+squared = math.pow(num, 2)
+print("The square of", num, "is", squared)
+
+print("Keep your face to the sunshine and you cannot see a shadow.")
